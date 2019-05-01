60417d9828037351585420a78aec9dd7497401e2
Arie Trouw <arie.trouw@xy.company>
2019-05-01 00:14:30 -0500

    License Check Updated Config

--- scripts/licensecheck.js
+++ scripts/licensecheck.js
@@ -6,6 +6,7 @@ const allowedLicense = [
   'BSD-2-Clause',
   'BSD-3-Clause',
   'Apache-2.0',
+  'Apache 2.0',
   'ISC',
   'Unlicense',
   'BSD*',
@@ -15,12 +16,16 @@ const allowedLicense = [
   'CC-BY-3.0'
 ]
 
+const excludePackages = [
+  '@apollographql/graphql-playground-html@1.6.6'
+]
+
 return new Promise((resolve, reject) => {
   checker.init({
     start: './',
     onlyAllow: allowedLicense.join(';'),
-    production: true,
-    direct: true
+    excludePackages: excludePackages.join(';'),
+    production: true
   }, function(err, packages) {
       if (err) {
           reject(err)
