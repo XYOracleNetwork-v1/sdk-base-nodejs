d37969056020851a881225489c5b9bcab0abfffc
Carter Harrison <carterjharrison@gmail.com>
2019-05-08 17:56:03 -0700

    add plugin interface

---
+++ src/plugin/index.ts
@@ -0,0 +1,28 @@
+
+export interface IXyoPluginWithConfig {
+  config: any,
+  plugin: IXyoPlugin
+}
+
+export interface IXyoPlugin {
+  getName(): string
+  getProvides(): string[]
+  getPluginDependencies(): string[]
+  initialize(deps: { [key: string]: any; }, config: any, graphql?: IXyoGraphQlDelegate): Promise<boolean>
+}
+
+export interface IXyoConfig {
+  port: number,
+  plugins: IXyoPluginConfig[],
+
+export interface IXyoPluginConfig {
+  packageName: string
+  config: any
+  path?: string
+}
+
+export interface IXyoGraphQlDelegate {
+  addType(type: string): void
+  addQuery(queryString: string): void
+  addResolver(query: string, resolver: any): void
+}
