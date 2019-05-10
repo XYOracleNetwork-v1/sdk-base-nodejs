8375507d2674d3f4546de08d15ac96b8d30a1ff8
Arie Trouw <arie.trouw@xy.company>
2019-05-10 11:42:35 -0700

    build/lint scripts

--- scripts/build.js
+++ scripts/build.js
@@ -1,16 +1,43 @@
+//this should only be run in the context of yarn so that the local bin folder is available
+
 const {spawnSync} = require('child_process')
 const chalk = require('chalk')
+const path = require('path')
+const fs = require('fs-extra')
+
+//the prefix makes this so the script will work for both the base build and any project that includes it
+let prefix = "node_modules/@xyo-network/sdk-base-nodejs/"
+
+if (!fs.existsSync(prefix)) {
+  prefix = ''
+}
 
 console.log(chalk.green('XY Typescript Build: Start'))
-console.log(chalk.gray('Compiling Typescript'))
 
-let buildResult = spawnSync('tsc', ['-b'], {
+let hooksResult = spawnSync('yarn', ['hooks'], {
+  stdio: 'inherit'
+})
+
+if (hooksResult.status != 0) {
+  return hooksResult.status
+}
+
+console.log(chalk.gray('Compiling Typescript'))
+let buildResult = spawnSync('tsc', ['-b', `${prefix}tsconfig.json`], {
   stdio: 'inherit'
 })
 
 if (buildResult.status != 0) {
   console.log(chalk.red(`XY Typescript Build: Failed`))
   return buildResult.status
+}
+
+let lintResult = spawnSync('yarn', ['lint'], {
+  stdio: 'inherit'
+})
+
+if (lintResult.status != 0) {
+  return lintResult.status
 }
 
 console.log(chalk.green(`XY Typescript Build: Complete`))
---
+++ scripts/lint.js
@@ -0,0 +1,26 @@
+//this should only be run in the context of yarn so that the local bin folder is available
+
+const {spawnSync} = require('child_process')
+const chalk = require('chalk')
+const path = require('path')
+const fs = require('fs-extra')
+
+//the prefix makes this so the script will work for both the base build and any project that includes it
+let prefix = "node_modules/@xyo-network/sdk-base-nodejs/"
+
+if (!fs.existsSync(prefix)) {
+  prefix = ''
+}
+
+console.log(chalk.green('XY Typescript Linting'))
+let buildResult = spawnSync('tslint', ['-c', `${prefix}tslint.json`, '-p', `${prefix}tsconfig.json`], {
+  stdio: 'inherit'
+})
+
+if (buildResult.status != 0) {
+  console.log(chalk.red(`XY Typescript Linting: Failed`))
+  return buildResult.status
+}
+
+console.log(chalk.green(`XY Typescript Linting: Complete`))
+return 0
