50348b51a188d7820b185dc05027d05002eea184
pllearns <thenewphilliplorenzo@outlook.com>
2019-05-23 13:31:47 -0700

    fix: fix strings for codacy review standard

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { XyoBase } from './xyo-base'
-export * from './log'
-export * from './plugin'
-export * from './xyo-plugin-providers'
+export { XyoBase } from "./xyo-base"
+export * from "./log"
+export * from "./plugin"
+export * from "./xyo-plugin-providers"
