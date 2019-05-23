3f3ce7994de123ace98811bec85ad3b2a96029f7
pllearns <thenewphilliplorenzo@outlook.com>
2019-05-23 13:39:24 -0700

    fix: tslint check

--- src/xyo-plugin-providers.ts
+++ src/xyo-plugin-providers.ts
@@ -1,14 +1,14 @@
 
 export class XyoPluginProviders {
-  static BOUND_WITNESS_COLLECTOR_STATS =        "BOUND_WITNESS_COLLECTOR_STATS"
-  static BOUND_WITNESS_INSERTER =               "BOUND_WITNESS_INSERTER"
-  static BLOCK_REPOSITORY_GET =                 "BLOCK_REPOSITORY_GET"
-  static BLOCK_REPOSITORY_ADD =                 "BLOCK_REPOSITORY_ADD"
-  static BLOCK_REPOSITORY_PUBLIC_KEY =          "BLOCK_REPOSITORY_PUBLIC_KEY"
-  static BLOCK_REPOSITORY_PUBLIC_GEOHASH =      "BLOCK_REPOSITORY_PUBLIC_GEOHASH"
+  static BOUND_WITNESS_COLLECTOR_STATS =        'BOUND_WITNESS_COLLECTOR_STATS'
+  static BOUND_WITNESS_INSERTER =               'BOUND_WITNESS_INSERTER'
+  static BLOCK_REPOSITORY_GET =                 'BLOCK_REPOSITORY_GET'
+  static BLOCK_REPOSITORY_ADD =                 'BLOCK_REPOSITORY_ADD'
+  static BLOCK_REPOSITORY_PUBLIC_KEY =          'BLOCK_REPOSITORY_PUBLIC_KEY'
+  static BLOCK_REPOSITORY_PUBLIC_GEOHASH =      'BLOCK_REPOSITORY_PUBLIC_GEOHASH'
 
-  static CHAIN_TRACER =                         "CHAIN_TRACER"
-  static DEBUG_BLOCK_GEN =                      "DEBUG_BLOCK_GEN"
-  static PAYMENT_STORE =                        "PAYMENT_STORE"
-  static QUERY =                                "QUERY"
+  static CHAIN_TRACER =                         'CHAIN_TRACER'
+  static DEBUG_BLOCK_GEN =                      'DEBUG_BLOCK_GEN'
+  static PAYMENT_STORE =                        'PAYMENT_STORE'
+  static QUERY =                                'QUERY'
 }
