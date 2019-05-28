2aa9bf26f0b064599ebe789107a62a942fa87ce5
Arie Trouw <arie.trouw@xy.company>
2019-05-28 16:04:37 -0700

    eslint config for scripts

--- scripts/hooks.js
+++ scripts/hooks.js
@@ -1,22 +1,24 @@
-const path = require('path')
-const fs = require('fs-extra')
-const chalk = require('chalk')
+#!/usr/bin/env node
+const path = require('path');
+const fs = require('fs-extra');
+const chalk = require('chalk');
 
-//the prefix makes this so the script will work for both the base build and any project that includes it
-let prefix = "node_modules/@xyo-network/sdk-base-nodejs/"
+// the prefix makes this so the script will work for both the base build and
+// any project that includes it
+let prefix = 'node_modules/@xyo-network/sdk-base-nodejs/';
 
 if (!fs.existsSync(prefix)) {
-  prefix = ''
+  prefix = '';
 }
 
-const srcHooksFolder = path.resolve(`${prefix}hooks`)
-const dstHooksFolder = path.resolve('.git/hooks')
+const srcHooksFolder = path.resolve(`${prefix}hooks`);
+const dstHooksFolder = path.resolve('.git/hooks');
 
-console.log(chalk.green(`XY Copy Hooks: Start`))
+console.log(chalk.green('XY Copy Hooks: Start'));
 fs.copy(srcHooksFolder, dstHooksFolder, {}, (err) => {
   if (err) {
-    console.error(chalk.red(`XY Copy Hooks: Failed`))
+    console.error(chalk.red('XY Copy Hooks: Failed'));
   } else {
-    console.log(chalk.green(`XY Copy Hooks: Complete`))
+    console.log(chalk.green('XY Copy Hooks: Complete'));
   }
-})
+});
