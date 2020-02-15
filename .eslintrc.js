59ff793d51526eb6fedc6a5925615480d2908b81
pllearns <thenewphilliplorenzo@outlook.com>
2020-02-14 17:04:40 -0800

    update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,8 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint', 'react', 'prettier'],
   extends: [
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
     'airbnb-typescript',
-    'plugin:prettier/recommended'
+
   ],
   parserOptions: {
     project: './tsconfig.json'
