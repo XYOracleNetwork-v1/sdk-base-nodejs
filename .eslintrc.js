2b8b11e95c447a0d96965762969253427d0a63b9
pllearns <thenewphilliplorenzo@outlook.com>
2020-02-14 17:07:02 -0800

    update eslint with correct plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,11 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'react', 'prettier'],
+  plugins: ['@typescript-eslint', 'react'],
   extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
     'airbnb-typescript',
-
   ],
   parserOptions: {
     project: './tsconfig.json'
